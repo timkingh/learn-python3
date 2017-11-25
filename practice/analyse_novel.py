fc51688059587719ff0d1499127758ff4eb2fd2a
timkingh.huang <timkingh.huang@rock-chips.com>
2017-11-25 17:06:44 +0800

    practice-1: analyse novel
    Signed-off-by: timkingh.huang <timkingh.huang@rock-chips.com>

---
+++ practice/analyse_novel.py
@@ -0,0 +1,34 @@
+﻿# with...as 自动调用close()方法
+with open('Cambridge_China_History_UTF-8.txt',encoding='utf-8',errors='ignore') as f:
+	cont = [line.strip() for line in f.readlines() if line.strip()]
+	
+#for c in cont[10:50]:
+#	print(c)
+   
+names = ['毛','周','邓','朱','费']   
+   
+def find_main_characters(num = 3):
+	novel = ''.join(cont)
+	count = []
+	for name in names:
+		count.append([name, novel.count(name)])
+		count.sort(key = lambda v : v[1], reverse=True)
+			
+	return count[:num]       
+	
+print(find_main_characters())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
