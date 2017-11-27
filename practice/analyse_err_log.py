c0a18ab1e7feab3c1e63c0c52837b1b83c66dfe3
timkingh.huang <timkingh.huang@rock-chips.com>
2017-11-27 11:55:07 +0800

    practice-2: analyse error log
    Signed-off-by: timkingh.huang <timkingh.huang@rock-chips.com>

---
+++ practice/analyse_err_log.py
@@ -0,0 +1,28 @@
+﻿# with...as 自动调用close()方法
+with open('err_log.txt',encoding='gbk',errors='ignore') as f:
+	cont = [line.strip() for line in f.readlines() if line.strip()]
+	
+for c in cont:
+	if c.find("input file path") != -1 or \
+		 c.find("error count") != -1:
+		print(c)
+   
+   
+   
+   
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
